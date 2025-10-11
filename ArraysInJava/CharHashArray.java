181f5c721286a328dc784abac3b3e08d1d6ec6ac
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2025-10-11 15:39:57 +0530

    modified change in code

--- ArraysInJava/CharHashArray.java
+++ ArraysInJava/CharHashArray.java
@@ -2,7 +2,7 @@
 public class CharHashArray {
     public static int maxFreq(String s, char c){
         if(s.length() < 0) return  0;
-     // we have taken 25 because we assume the string will be in lower case, we will have taken 256 if there is an upper case string
+     // we have taken 25 because we assume the string will be in complete lower case or in complete upper case, we will have taken 256 if there is a mixup of an upper case and lower case string
      int[]Hash = new int[256]; // 25 for lowercase
      for(int i = 0; i < s.length(); i++){
           Hash[s.charAt(i) - 'A']++; //  ( char - 'a') if sure the string will be in a lower case
@@ -12,6 +12,7 @@ public class CharHashArray {
     //main function
     public static void main(String[] args) {
         String s = "Abaabcaddas";
+        // String s = "AAABSCD";
         int result = maxFreq(s, 'A');
         System.out.println("The max freq of an character 'a' in a string is => " + result);
     }
