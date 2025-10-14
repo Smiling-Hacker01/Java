e6acae81ea294f8fdeb11f13d0f4e661ba2498c3
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2025-10-14 19:52:09 +0530

    code is to check wether the string is palindrome or not

---
+++ AllTypesQuesTechnical/Ques4.java
@@ -0,0 +1,23 @@
+//functions to check if the string is palindrome or not
+public class Ques4 {
+    public static String reverseString(String str, String newStr, int index){
+        if (index < 0) {
+            return newStr;
+        }
+        char currentChar = str.charAt(index);
+        return reverseString(str, newStr+currentChar, index-1);
+    }
+    public static boolean ispalindrome(String str){
+       String rev =  reverseString(str, "", str.length()-1);
+       if (rev.equals(str)) {
+          return true;
+       }
+       return false;
+    }
+    //main function
+    public static void main(String[] args){
+        String str = "vishal";
+       boolean result =  ispalindrome(str);
+       System.out.println(result);
+    }
+}
